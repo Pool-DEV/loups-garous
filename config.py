443ab71b723e7561652bff6d4eacbb51cadeec6f
Pool-DEV <40335015+Pool-DEV@users.noreply.github.com>
2024-12-22 15:17:01 +0100

    Ajout de la configuration du jeu et répartition des rôles

--- config.py
+++ config.py
@@ -1,2 +1,15 @@
-def bonj():
-    return 
+# Configuration file for the application
+
+# Number of players in the game
+NB_PLAYERS = 10
+
+# Roles repartition
+ROLES = {
+    "villagers": 2,
+    "werewolves": 3,
+    "seer": 1,
+    "witch": 1,
+    "hunter": 1,
+    "cupid": 1,
+    "thief": 1,
+}
