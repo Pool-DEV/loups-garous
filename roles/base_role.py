ce9833b399e2e6d23341d4204a5ff152abb04595
Pool-DEV <40335015+Pool-DEV@users.noreply.github.com>
2024-12-24 14:21:56 +0100

    Déplacement des fichiers pour le client, le protocole de message et le serveur dans la racine du projet.

--- roles/base_role.py
+++ roles/base_role.py
@@ -1,5 +1,5 @@
 class BaseRole:
-    def __init__(self, name, team, description):
+    def __init__(self, name, team, description, night_action):
         self.name = name
-        self.team = team  # "vil", "wolf", "neutral"
+        self.team = team # "vil", "wolf", "neutral"
         self.description = description
